52b355633301c9c82f6e87cdf565c485525a981c
Your Name <you@example.com>
2021-03-28 19:53:48 -0400

    setting time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 var tasks= {};
 
-// create task element
-var createTask= function(taskText, taskList) {
-    var taskLi= $("<li>").addClass("list-element");
-    var taskP= $("<textarea>").addClass("text-area").text(taskText);
+// // create task element
+// var createTask= function(taskText, taskList) {
+//     var taskLi= $("<li>").addClass("list-element");
+//     var taskP= $("<textarea>").addClass("text-area").text(taskText);
 
-    // append p element to parent li
-     taskLi.append(taskP);
+//     // append p element to parent li
+//      taskLi.append(taskP);
 
-    // append ul to the page
-    $("#list-" + taskList).append(taskP);
+//     // append ul to the page
+//     $("#list-" + taskList).append(taskP);
 
 
-    console.log(taskLi);
-    console.log(taskP);
-};
+//     console.log(taskLi);
+//     console.log(taskP);
+// };
 
-// save button is clicked
+// save button is clicked to corresponding task
 $("#saveBtn01").click(function() {
     var taskText= $("#modaltask01").val().trim();
     console.log(taskText);
@@ -32,13 +32,66 @@ $("#saveBtn03").click(function() {
     console.log(taskText);
     localStorage.setItem("1000 HR", taskText);
 });
+$("#saveBtn04").click(function() {
+    var taskText= $("#modaltask04").val().trim();
+    console.log(taskText);
+    localStorage.setItem("1100 HR",taskText);
+});
+$("#saveBtn05").click(function() {
+    var taskText= $("#modaltask05").val().trim();
+    console.log(taskText);
+    localStorage.setItem("1200 HR", taskText);
+});
+$("#saveBtn06").click(function() {
+    var taskText= $("#modaltask06").val().trim();
+    console.log(taskText);
+    localStorage.setItem("1300 HR", taskText);
+});
+$("#saveBtn07").click(function() {
+    var taskText= $("#modaltask07").val().trim();
+    console.log(taskText);
+    localStorage.setItem("1400 HR",taskText);
+});
+$("#saveBtn08").click(function() {
+    var taskText= $("#modaltask08").val().trim();
+    console.log(taskText);
+    localStorage.setItem("1500 HR", taskText);
+});
+$("#saveBtn09").click(function() {
+    var taskText= $("#modaltask09").val().trim();
+    console.log(taskText);
+    localStorage.setItem("1600 HR", taskText);
+});
+$("#saveBtn10").click(function() {
+    var taskText= $("#modaltask10").val().trim();
+    console.log(taskText);
+    localStorage.setItem("1700 HR", taskText);
+});
+$("#saveBtn11").click(function() {
+    var taskText= $("#modaltask11").val().trim();
+    console.log(taskText);
+    localStorage.setItem("1800 HR", taskText);
+});
 
 // save into local storage
 var saveTask= function(taskText) {
     localStorage.setItem("taskText", JSON.stringify(taskText));
 };
 
+var loadTask= function() {
+    tasks= JSON.parse(localStorage.getItem("taskText"));
+    console.log(tasks);
+};
+
+
+//    createTask();
 
 
 
-   createTask();
+ var rightNow= new Date();
+ $("#currentDay").html(rightNow);
+ console.log(rightNow);
+ console.log(typeof rightNow);
+
+var m= moment('2013-03-01', 'YYYY-MM-DD ');
+console.log(m);
